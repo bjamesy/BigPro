c4d53d7d318ebd3364e20ff8b906105bc445ef0f
James Ballanger <jamesballanger@Jamess-MacBook-Air.local>
2019-06-20 07:53:51 -0400

    post routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+
 
 const app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
